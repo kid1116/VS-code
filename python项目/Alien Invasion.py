0bd3ad71d6228514156ad5bd49ff92feb8a268d4
kid1116 <121206739@qq.com>
2026-01-30 21:38:19 +0800

    更新

--- python项目/Alien Invasion.py
+++ python项目/Alien Invasion.py
@@ -15,18 +15,16 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    #设置背景色(RGB)
-    bg_color = ai_settings.bg_color
-  
+
     # 开始游戏的主循环
     while True:
         #监视键盘和鼠标事件
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-
+                
         #绘制屏幕颜色
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
 
         #更新屏幕显示
         pygame.display.flip()
--- python项目/settings.py
+++ python项目/settings.py
@@ -2,4 +2,4 @@ class Settings():
     def __init__(self):
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (250, 245, 230)
+        self.bg_color = (250, 245, 230) #背景颜色(RGB)
