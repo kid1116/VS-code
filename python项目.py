d528332bb4b13574a48dbd3347437758574ae192
kid1116 <121206739@qq.com>
2026-01-30 17:17:33 +0800

    开始python小游戏项目

---
+++ python项目.py
@@ -0,0 +1,23 @@
+import sys
+
+import pygame
+
+def run_game():
+    # Initialize the game and create a screen object
+    pygame.init()
+    screen = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption("Simple Pygame Example")
+
+    # Main loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        # Fill the screen with a color (RGB)
+        screen.fill((0, 128, 255))
+
+        # Update the display
+        pygame.display.flip()
+
+run_game()
