ec0d3d20f7a8555d4cebd4213fc85b9f653e1ccf
kid1116 <121206739@qq.com>
2026-01-31 00:18:05 +0800

    更新

--- python项目/game_functions.py
+++ python项目/game_functions.py
@@ -7,21 +7,27 @@ def update_screen(ai_settings,screen,ship):
     ship.blitme()
     pygame.display.flip()
 
+def check_keydown_events(event,ship):
+    if event.key == pygame.K_RIGHT:
+         ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left =True
+
+def check_keyup_events(event,ship):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+           
 #响应键盘和鼠标事件
 def check_events(ship):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        
         elif event.type == pygame.KEYDOWN: #按下键盘
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left =True
-
+            check_keydown_events(event,ship)
         elif event.type == pygame.KEYUP: #松开键盘
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
-           
+            check_keyup_events(event,ship)
+              
+        
+
