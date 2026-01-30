f698b603ba196f14599ea809062a3d1c2b049edc
kid1116 <121206739@qq.com>
2026-01-30 22:27:17 +0800

    加入图片

--- python项目/Alien Invasion.py
+++ python项目/Alien Invasion.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import pygame
+import time
 
 from settings import Settings
 
@@ -11,22 +12,26 @@ print("开始游戏！")
 def run_game():
     # 初始化游戏并创建一个屏幕对象
     pygame.init()
-    ai_settings =Settings()
-    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    ai_settings =Settings()
 
+    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
+    screen_rect = screen.get_rect()
 
+    image = pygame.image.load("D:\VS code\python项目\ship.bmp")
+    ship_rect = image.get_rect()
+    ship_rect.center = screen_rect.center
+
+    screen.fill(ai_settings.bg_color)
+    screen.blit(image, ship_rect)
+    pygame.display.flip()
+    
+  
     # 开始游戏的主循环
     while True:
         #监视键盘和鼠标事件
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-                
-        #绘制屏幕颜色
-        screen.fill(ai_settings.bg_color)
 
-        #更新屏幕显示
-        pygame.display.flip()
-       
 run_game()
