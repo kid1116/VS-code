57bcecc858ffb1f1e4577bf232890b180e48df71
kid1116 <121206739@qq.com>
2026-02-02 21:58:32 +0800

    增加外星人

--- python项目/Alien_Invasion.py
+++ python项目/Alien_Invasion.py
@@ -5,10 +5,11 @@ from pygame.sprite import Group
 import game_functions as gf
 from settings import Settings
 from ship import Ship
+from alien import Alien
 
 #先清屏
 os.system("cls")
-print("开始游戏！")
+print("开始游戏！(按Q键退出)")
 
 def run_game():
     pygame.init()
@@ -17,14 +18,17 @@ def run_game():
     ai_settings =Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     ship = Ship(ai_settings,screen)
-
     bullets = Group() #创建子弹编组
+
+    alien = Alien(ai_settings,screen)
+    aliens =Group() #创建外星人编组
+    gf.create_fleet(ai_settings,screen,ship,aliens)
 
     #游戏主循环
     while True:
         gf.check_events(ai_settings,screen,ship,bullets) 
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings,screen,ship,bullets)
+        gf.update_screen(ai_settings,screen,ship,bullets,aliens)
         
 run_game()
--- python项目/settings.py
+++ python项目/settings.py
@@ -4,12 +4,12 @@ class Settings():
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (250, 245, 230) 
-        self.ship_speed_factor = 1.5
+        self.ship_speed_factor = 2
         
         #子弹设置
-        self.bullet_speed_factor = 0.7
+        self.bullet_speed_factor = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60,60,60
-        self.bullets_allowed = 3
+        self.bullets_allowed = 5
         
