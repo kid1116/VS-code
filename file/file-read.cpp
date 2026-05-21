712bffa1114c73121ea0a44d3f0484376e20bf3c
kid1116 <121206739@qq.com>
2026-05-21 21:43:45 +0800

    file practice

---
+++ file/file-read.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    ifstream myfile;
+    myfile.open("example.txt");
+    if (!myfile)
+    {
+        cerr << "Unable to open file";
+        exit(EXIT_FAILURE); // 终止程序
+    }
+
+    int account;
+    string name;
+    double balance;
+
+    while (myfile >> account >> name >> balance)
+    {
+        cout << account << " " << name << " " << balance << endl;
+    }
+
+    myfile.close(); // 关闭文件
+}
